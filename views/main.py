d2b86eaccebea0fcedeb2cc7b171a83914730aae
mayumiPc <mayumiobara089@gmail.com>
2022-10-09 13:46:50 +0900

    コンフリクト解消

--- views/main.py
+++ views/main.py
@@ -304,11 +304,8 @@ class Events(BaseEvents):
 			d = versionDialog.dialog()
 			d.Initialize()
 			r = d.Show()
-<<<<<<< Updated upstream
-=======
 		if selected >= constants.MENU_MATERIAL_OPEN:
 			return
->>>>>>> Stashed changes
 		if selected >= constants.MENU_URL_COPY:
 			obj = event.GetEventObject()
 			pyperclip.copy(obj.GetLabel(selected))
@@ -421,16 +418,10 @@ class Events(BaseEvents):
 		self.parent.menu.RegisterMenuCommand(context, "tempFile_open", subMenu=tmp)
 		for i,j in zip(urlLists, range(len(urlLists))):
 			self.i = i
-			self.j = j
 			openSubMenu.Append(constants.MENU_URL_OPEN + j,i)
 			copySubMenu.Append(constants.MENU_URL_COPY + j,i)
-<<<<<<< Updated upstream
-			#現在FOCUSがあたっているインデックス番号をfocusに代入しているので、これをannouncement[]に指定してfor文を回す
-=======
 			#添付ファイルが含まれたお知らせとインデックス番号を取得
->>>>>>> Stashed changes
 		for k,l in zip(self.parent.announcementData[focus], range(len(self.parent.announcementData))):
-			print(l)
 			#announcementDataの中の辞書が格納されたリストにアクセスできるのでmaterialsにアペンドされた辞書を取り出すことができる
 			tmp.Append(constants.MENU_MATERIAL_OPEN + l,k["name"])
 		self.parent.announcementList.PopupMenu(context, event)
