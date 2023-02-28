e9211cd3e2c0e64ec123f0e36ee3c260be7646a8
mayumiPc <mayumiobara089@gmail.com>
2023-03-01 07:21:35 +0900

    オルト+f4が押された時に完全終了しないように変更

--- views/main.py
+++ views/main.py
@@ -46,12 +46,10 @@ class MainView(BaseView):
 		)
 		self.InstallMenuEvent(Menu(self.identifier),self.events.OnMenuSelect)
 		self.courses = []
-		self.menu.hMenuBar.Enable(menuItemsStore.getRef("file_hide"), False)
 		self.service = self.getService()
 		if not self.service:
 			self.menu.hMenuBar.Enable(menuItemsStore.getRef("file_class_update"), False)
 			self.menu.hMenuBar.Enable(menuItemsStore.getRef("file_update"), False)
-
 			self.menu.hMenuBar.Enable(menuItemsStore.getRef("file_back"), False)
 			return
 
@@ -423,12 +421,11 @@ class Events(BaseEvents):
 		if event.CanVeto():
 			# Alt+F4が押された
 			if globalVars.app.config.getboolean("general", "minimizeOnExit", True):
-				self.exit()
+				self.hide()
 			else:
 				super().OnExit(event)
 				globalVars.app.tb.Destroy()
-			globalVars.app.tb.Destroy()
-			return
+				return
 
 	def hide(self):
 		self.parent.hFrame.Hide()
@@ -471,7 +468,6 @@ class Events(BaseEvents):
 		self.parent.description_data()
 		self.parent.DSCBOX.Clear()
 		self.parent.DSCBOX.Disable()
-		self.parent.menu.hMenuBar.Enable(menuItemsStore.getRef("file_hide"), True)
 		materials = self.parent.tempFiles(self.courseId)
 		materials = self.parent.workMaterials(materials)
 		self.parent.creator.GetPanel().Layout()
